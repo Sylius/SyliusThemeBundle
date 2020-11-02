4794ae16eb56df29a829117de1a32e988a6d7aec
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2020-11-02 12:23:10 +0100

    remove unused use

--- src/Translation/ThemeAwareTranslator.php
+++ src/Translation/ThemeAwareTranslator.php
@@ -15,7 +15,6 @@ namespace Sylius\Bundle\ThemeBundle\Translation;
 
 use Sylius\Bundle\ThemeBundle\Context\ThemeContextInterface;
 use Symfony\Component\HttpKernel\CacheWarmer\WarmableInterface;
-use Symfony\Component\Translation\MessageCatalogueInterface;
 use Symfony\Component\Translation\TranslatorBagInterface;
 use Symfony\Contracts\Translation\LocaleAwareInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
